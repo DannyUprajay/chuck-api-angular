e8b08c1a5fae3204a189c109faafbfe3c24c07e8
Danny <danny.uprajay@gmail.com>
2023-09-11 11:46:10 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
+// Fonctionnalités
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
+// Composants & modules
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ChuckComponent } from './chuck/chuck.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SecondPageComponent,
+    NavbarComponent,
+    ChuckComponent
   ],
   imports: [
+    //Fonctionnalité
     BrowserModule,
+    HttpClientModule,
+
+    //Composant & Module
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/chuck/chuck.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chuck',
+  templateUrl: './chuck.component.html',
+  styleUrls: ['./chuck.component.css']
+})
+export class ChuckComponent {
+
+}
